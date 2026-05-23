f4da0404ec309f12ba2e1b3df058424c7af7209d
juanselospi <juanselospi@gmail.com>
2026-05-22 20:50:15 -0600

    Revision avances pt2

---
+++ Trabajo en clase/Semana-10/TClase-13/Server/LegoServer.h
@@ -0,0 +1,40 @@
+#ifndef LegoServer_h
+#define LegoServer_h
+
+#include <string>
+#include "Socket.h"
+#include "FileSystem.h"
+#include <sys/socket.h>
+
+class LegoServer {
+   private:
+      Socket serverSocket;
+      FileSystem fileSystem;
+      std::string bindIp;
+      int port;
+
+      std::string readHttpRequest( VSocket * );
+      std::string processRequest( const std::string & );
+      std::string buildHttpResponse( const std::string &,
+                                     const std::string & = "200 OK",
+                                     const std::string & = "text/html; charset=UTF-8" );
+
+      std::string getPathFromRequest( const std::string & );
+      std::string getQueryParam( const std::string &, const std::string & );
+
+      std::string handleIndex();
+
+      std::string handleList( const std::string & );
+
+      void handleClient( VSocket * );
+      std::string handleBitacora();
+      std::string handleBitacoraCliente();
+      std::string handleListNachos(const std::string &path);
+      std::string handleIndexNachos();
+
+   public:
+      LegoServer( const std::string &, int );
+      void run();
+};
+
+#endif
